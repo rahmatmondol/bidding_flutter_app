094f543621772d679712052f4260f1dadc865738
Shazzad Hossain <134483654+shazzadshojol@users.noreply.github.com>
2025-02-18 01:15:41 +0600

    Delete lib directory

--- lib/app/services/text_enum.dart
+++
@@ -1,8 +0,0 @@
-class TextEnum {
-  static const String noInternetConnection = 'no internet connection';
-  static const String serverNotResponding = 'server not responding';
-  static const String someThingWentWorng = 'something went wrong';
-  static const String apiNotFound = 'api not found';
-  static const String serverError = 'Server error';
-  static const String urlNotFound = 'Url not found';
-}
